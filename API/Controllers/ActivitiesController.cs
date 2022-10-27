38859657a87db020acd9db2b7e26e3b9a6135b8d
Julio Santana <juliosantana.rp@hotmail.com>
2022-10-27 13:20:03 +0100

    42

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
 
 
 namespace API.Controllers
@@ -19,10 +20,23 @@ namespace API.Controllers
             return await Mediator.Send(new Details.Query{Id = id});
         }
 
-        [HttpDelete("{id}")]
-        public ActionResult<Activity> DeleteActivity(Guid id)
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
         {
-            return Ok();
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+           activity.Id = id;
+           return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
 
     }
